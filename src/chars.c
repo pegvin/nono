a33d8553846005b3d915cf7111ec34836693d889
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-06-05 19:07:52 +0530

    Remove Stuff

--- src/chars.c
+++ src/chars.c
@@ -1,22 +1,5 @@
 /**************************************************************************
- *   chars.c  --  This file is part of GNU nano.                          *
- *                                                                        *
- *   Copyright (C) 2001-2011, 2013-2022 Free Software Foundation, Inc.    *
- *   Copyright (C) 2016-2021 Benno Schulenberg                            *
- *                                                                        *
- *   GNU nano is free software: you can redistribute it and/or modify     *
- *   it under the terms of the GNU General Public License as published    *
- *   by the Free Software Foundation, either version 3 of the License,    *
- *   or (at your option) any later version.                               *
- *                                                                        *
- *   GNU nano is distributed in the hope that it will be useful,          *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty          *
- *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
- *   See the GNU General Public License for more details.                 *
- *                                                                        *
- *   You should have received a copy of the GNU General Public License    *
- *   along with this program.  If not, see http://www.gnu.org/licenses/.  *
- *                                                                        *
+ *   chars.c  --  This file is part of nono.                              *
  **************************************************************************/
 
 #include "prototypes.h"
