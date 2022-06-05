587982bf112fdce78b42944ba96b0e5afdb0c06c
Aditya Mishra <75035219+DEVLOPRR@users.noreply.github.com>
2022-06-05 18:02:23 +0530

    Remove Stuff

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Generate configure & friends for GIT users.
 
-gnulib_url="git://git.sv.gnu.org/gnulib.git"
-gnulib_hash="34a93be8aa8a15f5959a1b671ad1285a5d552f4b"
+gnulib_url="git://git.sv.gnu.org/gnulib.git" # GNU Lib Git URL
+gnulib_hash="34a93be8aa8a15f5959a1b671ad1285a5d552f4b" # GNU Lib Commit Hash
 
 modules="
 	futimens
@@ -34,6 +34,8 @@ if [ ! -d "gnulib" ]; then
 	git clone --depth=1111 ${gnulib_url}
 fi
 cd gnulib >/dev/null || exit 1
+
+# Check If Current Hash is not equal to the repository we cloned, if not then pull the latest changes
 curr_hash=$(git log -1 --format=%H)
 if [ "${gnulib_hash}" != "${curr_hash}" ]; then
 	echo "Pulling..."
